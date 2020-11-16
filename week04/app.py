7408d462ae367ae8478f698ec4ebbd886cb1cbf0
eunjoolee <dmswn629@gmail.com>
2020-11-16 20:52:11 +0900

    0852

--- week04/app.py
+++ week04/app.py
@@ -10,7 +10,7 @@ def test_get():
    title_receive = request.args.get('title_give')
    print(title_receive)
    return jsonify({'result':'success', 'msg': '이 요청은 GET! 완료'})
- #0833333
+ #gygygy고쳤다
 @app.route('/test', methods=['POST'])
 def test_post():
    title_receive = request.form['title_give2']
